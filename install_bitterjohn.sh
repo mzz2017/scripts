fb33cc47f8dd6e5563aaf577d43a4e2363daf321
mzz <mzz@tuta.io>
2022-06-21 21:42:07 +0800

    Update install_bitterjohn.sh

--- install_bitterjohn.sh
+++ install_bitterjohn.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 enable_bbr() {
+  sed -i '/net.core.default_qdisc=/d' /etc/sysctl.conf
+  echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+  sysctl net.core.default_qdisc=fq
+
   sed -i '/net.ipv4.tcp_congestion_control=/d' /etc/sysctl.conf
   echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
   sysctl net.ipv4.tcp_congestion_control=bbr
@@ -39,7 +43,11 @@ download_and_install() {
   set -ex
   temp_file=$(mktemp /tmp/BitterJohn.XXXXXXXXX)
   trap "rm -f '$temp_file'" exit
-  version=$(curl -s https://api.github.com/repos/e14914c0-6759-480d-be89-66b7b7676451/BitterJohn/releases/latest|jq -r .tag_name)
+  if [ -z "$1" ]; then
+    version=$(curl -s https://api.github.com/repos/e14914c0-6759-480d-be89-66b7b7676451/BitterJohn/releases/latest|jq -r .tag_name)
+  else
+    version=$1
+  fi
   curl -L "https://github.com/e14914c0-6759-480d-be89-66b7b7676451/BitterJohn/releases/latest/download/BitterJohn_${PLATFORM}_${ARCH}_${version:1}" -o "${temp_file}"
   chmod +x "${temp_file}"
   "${temp_file}" install -g
@@ -47,4 +55,4 @@ download_and_install() {
 }
 
 enable_bbr
-download_and_install
+download_and_install $1
